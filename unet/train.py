a35e3977ed4b966364503ee5c3aeec72364ca2eb
Brennan Gebotys <gebob19.git@gmail.com>
2018-09-26 11:12:19 -0400

    updated params

--- unet/train.py
+++ unet/train.py
@@ -14,7 +14,7 @@ from model import unet
 import sys
 sys.path.insert(0, '..')
 from helpers import TrainValTensorBoard, get_data, createGenerator
-from losses import dice_loss, bce_dice_loss, bce_logdice_loss
+from losses import dice_loss, bce_dice_loss, bce_logdice_loss, binary_crossentropy
 
 from keras import backend as K
 sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, device_count = {'CPU' : 16, 'GPU' : 1}))
@@ -34,12 +34,12 @@ xtrain, xval, ytrain, yval, dtrain, dval, idtrain, idval = get_data(data_path)
 H, W, C = 256, 256, 1
 
 learning_rate = 3e-5
-BATCH_SIZE = 30
+BATCH_SIZE = 64
 EPOCHS = 100
 
-filter_sizes = [32, 64, 128, 256]
-bn_fsize = 64
-loss = dice_loss
+filter_sizes = [128, 256, 512, 1024]
+bn_fsize = 1024
+loss = binary_crossentropy
 
 model = unet((H, W, C), filter_sizes, bn_fsize, learning_rate, loss)
 
@@ -71,27 +71,27 @@ callbacks = [lr_plat, early_stop, m_checkpoint, tb]
 # In[83]:
 
 
-# attempt to over train model first => evaulate its capacity
-model.fit([xtrain, dtrain], ytrain, batch_size=BATCH_SIZE,
-#         steps_per_epoch=np.ceil(float(len(xtrain)) / float(BATCH_SIZE)),
-        epochs=EPOCHS,
-        verbose=1,
-        callbacks=callbacks,
-        validation_data=([xval, dval], yval))
-#         validation_steps=np.ceil(float(len(xval)) / float(BATCH_SIZE)))
+# # attempt to over train model first => evaulate its capacity
+# model.fit([xtrain, dtrain], ytrain, batch_size=BATCH_SIZE,
+# #         steps_per_epoch=np.ceil(float(len(xtrain)) / float(BATCH_SIZE)),
+#         epochs=EPOCHS,
+#         verbose=1,
+#         callbacks=callbacks,
+#         validation_data=([xval, dval], yval))
+# #         validation_steps=np.ceil(float(len(xval)) / float(BATCH_SIZE)))
 
 
 # In[ ]:
 
 
 # fit with depth dimension
-# model.fit_generator(generator=createGenerator(xtrain, dtrain, ytrain),
-#                     steps_per_epoch=np.ceil(float(len(xtrain)) / float(BATCH_SIZE)),
-#                     epochs=EPOCHS,
-#                     verbose=1,
-#                     callbacks=callbacks,
-#                     validation_data=([xval, dval], yval), 
-#                     validation_steps=np.ceil(float(len(xval)) / float(BATCH_SIZE)))
+model.fit_generator(generator=createGenerator(xtrain, dtrain, ytrain),
+                    steps_per_epoch=np.ceil(float(len(xtrain)) / float(BATCH_SIZE)),
+                    epochs=EPOCHS,
+                    verbose=1,
+                    callbacks=callbacks,
+                    validation_data=([xval, dval], yval), 
+                    validation_steps=np.ceil(float(len(xval)) / float(BATCH_SIZE)))
 
 
 # In[ ]:
